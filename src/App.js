89a3d097c1f5c0b86670143351104150b8e4ec4c
R4chel <rae84@cornell.edu>
2023-01-13 19:03:51 -0500

    replace one problem with another

--- src/App.js
+++ src/App.js
@@ -9,25 +9,28 @@ function App() {
     let degree = 3;
     let coefficients = [];
     let r,g,b;
-    const resolution = 1000;
+    const resolution = 200;
     const minCoefficient = 0.001;
     const maxCoefficient = 5;
-    const maxDelta = 0.01;
+    const maxDelta = 0.1;
     const canvasWidth = 400;
     const setup = (p5, canvasParentRef) => {
 
         p5.createCanvas(500, canvasWidth, p5.WEBGL).parent(canvasParentRef);
         for(let i = 0; i < degree; i++){
-            coefficients.push(p5.random(minCoefficient, maxCoefficient));
+            coefficients.push(p5.round(p5.random(minCoefficient, maxCoefficient), precision));
         }
+        
         r = p5.floor(p5.random(256));
         g = p5.floor(p5.random(256));
         b = p5.floor(p5.random(256));
         p5.background(0);
     };
     
+    const precision = 4;
     const colorDelta = 2;
     const draw = p5 => {
+        p5.background(0);
         p5.stroke(r,g,b);
         p5.noFill();
 
@@ -38,7 +41,7 @@ function App() {
             let x = p5.map(i, 0, resolution, xMin, xMax)
             let y = 0;
             for (let j = 0; j < coefficients.length; j++) {
-                y += coefficients[j] * x ** j
+                y += coefficients[j] * x ** j;
             }
             x = p5.map(x, xMin, xMax, 0, canvasWidth);
             p5.curveVertex(x,y);
@@ -48,7 +51,8 @@ function App() {
         r = clampedUpdate(p5, r, colorDelta, 0, 256);
         g = clampedUpdate(p5, g, colorDelta, 0, 256);
         b = clampedUpdate(p5, b, colorDelta, 0, 256);
-        coefficients = coefficients.map(x => x + p5.random(-maxDelta, maxDelta));
+        coefficients = coefficients.map(x => x + p5.round(p5.random(-maxDelta, maxDelta)),precision);
+        console.log(coefficients)
 
     };
     
